a46cd50882f9e5cc293fa47b2201e666deb4ad9a
Oberdan Marinho <oberdanmarinho@gmail.com>
2024-03-25 09:54:07 -0300

    Implementação de paginação nos contatos

--- AgendaContatos/Controllers/ContatoController.cs
+++ AgendaContatos/Controllers/ContatoController.cs
@@ -4,103 +4,103 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AgendaContatos.Controllers
 {
-    public class ContatoController : Controller
-    {
-        private readonly IContatoRepositorio _contatoRepositorio;
+	public class ContatoController : Controller
+	{
+		private readonly IContatoRepositorio _contatoRepositorio;
 
-        public ContatoController(IContatoRepositorio contatoRepositorio)
-        {
-            _contatoRepositorio = contatoRepositorio;
-        }
+		public ContatoController(IContatoRepositorio contatoRepositorio)
+		{
+			_contatoRepositorio = contatoRepositorio;
+		}
 
-        public IActionResult Index()
-        {
-            List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+		public IActionResult Index()
+		{
+			List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
 
-            return View(contatos);
-        }
+			return View(contatos);
+		}
 
-        public IActionResult Cadastrar()
-        {
-            return View();
-        }
+		public IActionResult Cadastrar()
+		{
+			return View();
+		}
 
-        public IActionResult DeletarRegistro(int id)
-        {
-            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
-            return View(contato);
-        }
+		public IActionResult DeletarRegistro(int id)
+		{
+			ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+			return View(contato);
+		}
 
-        public IActionResult Excluir(int id)
-        {
-            try
-            {
-                bool excluido = _contatoRepositorio.Excluir(id);
+		public IActionResult Excluir(int id)
+		{
+			try
+			{
+				bool excluido = _contatoRepositorio.Excluir(id);
 
-                if(excluido)
-                {
-                    TempData["MensagemSucesso"] = "Contato excluído com sucesso!";
-                }
-                else
-                {
-                    TempData["MensagemErro"] = "Erro ao excluír contato";
-                }
+				if (excluido)
+				{
+					TempData["MensagemSucesso"] = "Contato excluído com sucesso!";
+				}
+				else
+				{
+					TempData["MensagemErro"] = "Erro ao excluír contato";
+				}
 
-                return RedirectToAction("Index");
-            }
-            catch (System.Exception erro)
-            {
-                TempData["MensagemErro"] = $"Erro ao excluir contato: {erro.Message}";
-                return RedirectToAction("Index");
-            }
-        }
+				return RedirectToAction("Index");
+			}
+			catch (System.Exception erro)
+			{
+				TempData["MensagemErro"] = $"Erro ao excluir contato: {erro.Message}";
+				return RedirectToAction("Index");
+			}
+		}
 
-        public IActionResult Editar(int id)
-        {
-            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
-            return View(contato);
-        }
+		public IActionResult Editar(int id)
+		{
+			ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+			return View(contato);
+		}
 
-        [HttpPost]
-        public IActionResult Cadastrar(ContatoModel contato)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    _contatoRepositorio.Adicionar(contato);
-                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
-                    return RedirectToAction("Index");
-                }
+		[HttpPost]
+		public IActionResult Cadastrar(ContatoModel contato)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					_contatoRepositorio.Adicionar(contato);
+					TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
+					return RedirectToAction("Index");
+				}
 
-                return View(contato);
-            }
-            catch (System.Exception erro)
-            {
-                TempData["MensagemErro"] = $"Erro ao cadastraro contato: {erro.Message}";
-                return RedirectToAction("Index");
-            }
-        }
+				return View(contato);
+			}
+			catch (System.Exception erro)
+			{
+				TempData["MensagemErro"] = $"Erro ao cadastraro contato: {erro.Message}";
+				return RedirectToAction("Index");
+			}
+		}
 
-        [HttpPost]
-        public IActionResult Editar(ContatoModel contato)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    _contatoRepositorio.Editar(contato);
-                    TempData["MensagemSucesso"] = "Contato atualizado com sucesso";
-                    return RedirectToAction("Index");
-                }
+		[HttpPost]
+		public IActionResult Editar(ContatoModel contato)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					_contatoRepositorio.Editar(contato);
+					TempData["MensagemSucesso"] = "Contato atualizado com sucesso";
+					return RedirectToAction("Index");
+				}
 
-                return View(contato);
-            }
-            catch (System.Exception erro)
-            {
-                TempData["MensagemErro"] = $"Erro ao atualizar o contato: {erro.Message} ";
-                throw;
-            }
-        }
-    }
+				return View(contato);
+			}
+			catch (System.Exception erro)
+			{
+				TempData["MensagemErro"] = $"Erro ao atualizar o contato: {erro.Message} ";
+				throw;
+			}
+		}
+	}
 }
--- AgendaContatos/obj/Debug/net8.0/AgendaContatos.AssemblyInfo.cs
+++ AgendaContatos/obj/Debug/net8.0/AgendaContatos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e96e02d1558199c5e55cba1083abf7357bd7717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9b18f08e41bbf5a7ae8206e999da74c7613ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
