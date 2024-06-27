dda7f99edcb967c9cb9a677f60e93b481aa5d247
Oberdan Marinho <oberdanmarinho@gmail.com>
2024-06-27 09:26:13 -0300

    Correção ambiente Pt2

--- AgendaContatos/obj/Debug/net8.0/AgendaContatos.AssemblyInfo.cs
+++ AgendaContatos/obj/Debug/net8.0/AgendaContatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b5ce5c4cd53f76f4b0f3c76dd55351eed9dd29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6218c5757976c2fa15644ee3be8f348ef70b524")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
