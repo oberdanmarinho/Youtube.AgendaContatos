01c04aca713d334044a15bd6d80be2683e12fc88
Oberdan Marinho <oberdanmarinho@gmail.com>
2024-08-18 19:18:58 -0300

    tentativa de correção de gravação de novo usuário

--- AgendaContatos/Models/AlterarSenhaModel.cs
+++ AgendaContatos/Models/AlterarSenhaModel.cs
@@ -6,13 +6,13 @@ public class AlterarSenhaModel
 {
     public int Id { get; set; }
 
-    [Required(ErrorMessage ="Digite a senha atual do usuaário.")]
+    [Required(ErrorMessage = "Digite a senha atual do usuário.")]
     public string SenhaAtual { get; set; }
 
-    [Required(ErrorMessage = "Digite a nova senha do usuaário.")]
+    [Required(ErrorMessage = "Digite a nova senha do usuário.")]
     public string NovaSenha { get; set; }
-    
-    [Required(ErrorMessage = "Confirme a nova senha do usuaário.")]
-    [Compare("NovaSenha", ErrorMessage ="A senha não confere com a nova senha.")]
+
+    [Required(ErrorMessage = "Confirme a nova senha do usuário.")]
+    [Compare("NovaSenha", ErrorMessage = "A senha não confere com a nova senha.")]
     public string ConfirmarNovaSenha { get; set; }
 }
